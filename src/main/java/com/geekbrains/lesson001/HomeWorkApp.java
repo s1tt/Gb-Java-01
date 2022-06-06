2f98585d00c9553e091316727caa20cd0f3224f5
Sergey Alekseev <nec13@mail.ru>
2022-06-06 17:32:33 +0300

    Homework #1

--- src/main/java/com/geekbrains/lesson001/HomeWorkApp.java
+++ src/main/java/com/geekbrains/lesson001/HomeWorkApp.java
@@ -13,6 +13,7 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
     public static void checkSumSign() {
         int a = 2;
         int b = 3;
@@ -22,6 +23,7 @@ public class HomeWorkApp {
             System.out.println("The sum of the numbers is negative");
         }
     }
+
     public static void printColor() {
         int value = 6;
         if (value <= 0) {
@@ -32,6 +34,7 @@ public class HomeWorkApp {
             System.out.println("Green");
         }
     }
+
     public static void compareNumbers(){
         int a = 3;
         int b = 4;
